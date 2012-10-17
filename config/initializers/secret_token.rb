cce4664db9c0f6bc37b351af5ef265fbe6769f48
Arian <razzaghi2012@gmail.com>
2012-10-16 20:41:04 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TranslationApp::Application.config.secret_token = '681c71aa4d9b94a76c2f6dd76279f993c9f438fc59c2a7a375444d3c51a1f24612eb2cde5dc7833e0b3ac6ec2dd8688b501c85c8ce16a8896af6cd01cfcae218'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TranslationApp::Application.config.session_store :cookie_store, key: '_translation_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TranslationApp::Application.config.session_store :active_record_store
